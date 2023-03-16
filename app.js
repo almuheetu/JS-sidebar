9584003f36289a7fd6088337acf91d0f8ee0318f
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-03-16 20:47:52 +0600

    sidebar customize

---
+++ app.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function() {
+  
+});
